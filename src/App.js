00b49af42d78f718918ed07314efac0833b6014d
Brandon-Harris <brandonharris177@gmail.com>
2019-09-10 11:14:29 -0600

    todolist visible

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {useReducer} from 'react';
 import {intialState, reducer} from './reducers/reducer'
 import './App.css';
+import TodoList from './components/ToDoComponents/TodoList'
 
 function App() {
   const [state, dispatch] = useReducer(reducer, intialState)
   return (
     <div className="App">
       <h1>still works</h1>
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/ToDoComponents/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ToDoList = (props) => {
+    return (
+        <>
+        ToDoList
+        </>
+    );
+};
+
+export default ToDoList
