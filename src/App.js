dd373664d75a301a24960a034cbe03b0849802bb
Brandon-Harris <brandonharris177@gmail.com>
2019-09-10 11:03:24 -0600

    reducer built

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useReducer} from 'react';
+import {intialState, reducer} from './reducers/reducer'
 import './App.css';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, intialState)
   return (
     <div className="App">
+      <h1>still works</h1>
     </div>
   );
 }
---
+++ src/reducers/reducer.js
@@ -0,0 +1,15 @@
+export const intialState = {
+   todoItems: [
+        {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+        }
+    ]
+};
+
+export function reducer (state, action) {
+    switch (action.type) {
+        default: return state;
+    }
+}
